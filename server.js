992eb2db5054e84734c56a05356bac3b20ca9a8b
Ryan Wilson <ryanswilson1@gmail.com>
2021-01-14 10:56:16 -0500

    updated db and server

--- server.js
+++ server.js
@@ -15,10 +15,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/desolate-dusk-73252',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 
 app.use(require("./routes/api.js"));
